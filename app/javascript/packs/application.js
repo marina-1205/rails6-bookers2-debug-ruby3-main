0d6271739a5c61a5d460fcc7fdbee33daff24b79
marina-1205 <ec2-user@ip-172-31-30-171.ap-northeast-1.compute.internal>
2023-07-14 07:04:12 +0000

    5段階評価の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,16 @@ import "channels"
 import "jquery"
 import "popper.js"
 import "bootstrap"
-import "../stylesheets/application" 
+import "../stylesheets/application"
+
+window.$ = window.jQuery = require('jquery');
+
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
 
 Rails.start()
 Turbolinks.start()
